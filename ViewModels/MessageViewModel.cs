9a6bb3ec4142e21d3cc5c457deb4cee184c48096
Nate Bross <nate.bross@flpent.com>
2012-07-07 00:45:37 -0500

    added view models and RelayCommand

---
+++ ViewModels/MessageViewModel.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+
+namespace MailTools.ViewModels
+{
+    public class MessageViewModel: ViewModelBase
+    {
+        private ObservableCollection<string> _toAddresses;
+        public ObservableCollection<String> ToAddresses
+        {
+            get { return _toAddresses; }
+            set
+            {
+                _toAddresses = value;
+                NotifyPropertyChanged("ToAddresses");
+            }
+        }
+
+        private ObservableCollection<string> _ccAddresses;
+        public ObservableCollection<String> CarbonCopyAddresses
+        {
+            get { return _ccAddresses; }
+            set
+            {
+                _ccAddresses = value;
+                NotifyPropertyChanged("CarbonCopyAddresses");
+            }
+        }
+
+        private ObservableCollection<string> _bccAddresses;
+        public ObservableCollection<String> BlindCarbonCopyAddresses
+        {
+            get { return _bccAddresses; }
+            set
+            {
+                _bccAddresses = value;
+                NotifyPropertyChanged("BlindCarbonCopyAddresses");
+            }
+        }
+
+
+        private string _subject;
+        public String Subject
+        {
+            get { return _subject; }
+            set
+            {
+                _subject = value;
+                OnPropertyChanged("Subject");
+            }
+        }
+
+        private string _body;
+        public String Body
+        {
+            get { return _body; }
+            set
+            {
+                _body = value;
+                OnPropertyChanged("Body");
+            }
+        }
+
+        private bool _isHtmlBody;
+        public Boolean IsHtmlBody
+        {
+            get { return _isHtmlBody; }
+            set
+            {
+                _isHtmlBody = value;
+                OnPropertyChanged("IsHtmlBody");
+            }
+        }
+
+    }
+}
---
+++ ViewModels/SettingsViewModel.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+
+namespace MailTools.ViewModels
+{
+    public class SettingsViewModel : ViewModelBase
+    {
+        private string _smtpServer;
+        public String SmtpServer
+        {
+            get { return _smtpServer; }
+            set
+            {
+                _smtpServer = value;
+                OnPropertyChanged("SmtpServer");
+            }
+        }
+
+        private int _smtpPort;
+        public int SmtpPort
+        {
+            get { return _smtpPort; }
+            set
+            {
+                _smtpPort = value;
+                OnPropertyChanged("SmtpPort");
+            }
+        }
+
+        private bool _useSsl;
+        public bool UseSsl
+        {
+            get { return _useSsl; }
+            set
+            {
+                _useSsl = value;
+                OnPropertyChanged("UseSsl");
+            }
+        }
+
+        private string _fromAddress;
+        public String FromAddress
+        {
+            get { return _fromAddress; }
+            set
+            {
+                _fromAddress = value;
+                OnPropertyChanged("FromAddress");
+            }
+        }
+
+        private string _replyAddress;
+        public String ReplyAddress
+        {
+            get { return _replyAddress; }
+            set
+            {
+                _replyAddress = value;
+                OnPropertyChanged("ReplyAddress");
+            }
+        }
+    }
+}
