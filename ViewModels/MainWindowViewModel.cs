3f86ec3d4266023e2a4d42b46a816e01f5c0a084
Nate Bross <nate.bross@flpent.com>
2012-07-09 16:34:50 -0700

    Changed the layout on the mage window, removed extra margin

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
+using System.Windows.Input;
 
 namespace MailTools.ViewModels
 {
@@ -29,11 +31,41 @@ namespace MailTools.ViewModels
             }
         }
 
-        private string _log;
-        public String Log
+        private ObservableCollection<string> _log;
+        public ObservableCollection<String> Log
         {
             get { return _log; }
             set { _log = value; OnPropertyChanged("Log"); }
+        }
+
+        public MainWindowViewModel() { Log = new ObservableCollection<string>(); }
+
+        public ICommand SendMessage
+        {
+            get { return new RelayCommand((x) => { this.Send(); }); }
+        }
+
+        protected void Send()
+        {
+            try
+            {
+                var smtp = new System.Net.Mail.SmtpClient(SettingsVM.SmtpServer, SettingsVM.SmtpPort);
+                smtp.EnableSsl = SettingsVM.UseSsl;
+
+                var mm = new System.Net.Mail.MailMessage(SettingsVM.FromAddress, MessageVM.ToAddresses, MessageVM.Subject, MessageVM.Body);
+                mm.IsBodyHtml = MessageVM.IsHtmlBody;
+                mm.CC.Add(MessageVM.CarbonCopyAddresses);
+                mm.Bcc.Add(MessageVM.BlindCarbonCopyAddresses);
+                mm.ReplyTo = new System.Net.Mail.MailAddress(SettingsVM.ReplyAddress);
+
+                smtp.Send(mm);
+
+                Log.Add("Message sent successfully.");
+            }
+            catch (Exception ex)
+            {
+                Log.Add("Exception: " + ex.Message);
+            }
         }
     }
 }
